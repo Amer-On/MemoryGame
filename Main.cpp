4e0ad261fac6dbedee671c4808df4f4c29b63750
Amer-On <a20051772@yandex.ru>
2021-10-17 03:56:52 +0300

    Card by card choise, code refactor, bug fixes

--- Main.cpp
+++ Main.cpp
@@ -13,89 +13,56 @@ void ClearScreen() {
 	}
 }
 
-
+// parsing int numbers from the strings to avoid some problems with inputs
 int readIntFromString(string text) {
 	int number = 0;
+	bool hasNumber = false;
+
 	for (int i = 0; i < text.size(); i++) {
-		if (isdigit(text[i]))
+		if (isdigit(text[i])) {
 			number = 10 * number + (int) text[i] - 48;
+			hasNumber = true;
+		}
 		else
 			break;
 	}
+	if (not hasNumber)
+		throw runtime_error("No number found");
 	return number;
 }
 
-//unfinished, to do
-vector<int> readIntFromString(string text, int numbersAmount) {
-	vector<int> v;
-	int number;
-	int currentId = 0;
-	bool numberStarted = false;
-
-	for (int j = 0; j < numbersAmount; j++) {
-		number = 0;
-		for (int i = currentId; i < text.size(); i++) {
-			if (isdigit((char) text[i]) and not numberStarted) {
-				number = 10 * number + (int) text[i] - 48;
-				cout << numberStarted << "  ";
-				numberStarted = true;
-			} else {
-				numberStarted = false;
-				currentId = i + 1;
-				cout << "YEAH";
-				break;
-			}
-		}
-		cout << currentId << " ";
-		cout << numberStarted << "  ";
-		numberStarted = false;
-		v.push_back(number);
-	}
-	cout << "\n\n";
-	return v;
-}
-
-
-
+// the field class, which rules the game
 class Field {
 private:
 	vector<vector<int> > gameField;
 	vector<vector<int> > actualGameField;
-	int height;
-	int width;
+	int height, width;
 
 	vector<int> cards;
 	vector<int> allCards;
-	int pairsAmount;
-
-	bool allCardsFound;
-
+	
+	int x1, y1, x2, y2;
 
 public:
 	Field(int fieldHeight, int fieldWidth) {
 		height = fieldHeight;
 		width = fieldWidth;
-		pairsAmount = fieldWidth * fieldHeight / 2;
-		allCardsFound = false;
 
 		// generate cards
-		for (int i = 0; i < pairsAmount; i++) {
+		for (int i = 0; i < fieldWidth * fieldHeight / 2; i++) {
 			cards.push_back(i + 1);
 			cards.push_back(i + 1);
 			allCards.push_back(i + 1);
 		}
 
-
 		// generate field
 		vector<int> row;
 		vector<int> rowForAct;
-		int card;
 
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				card = takeCard();
 				rowForAct.push_back(0);
-				row.push_back(card);
+				row.push_back(takeCard());
 			}
 			actualGameField.push_back(rowForAct);
 			gameField.push_back(row);
@@ -104,15 +71,11 @@ public:
 	}
 
 	int takeCard(int position) {
-		int card = cards[position];
 		cards.erase(cards.begin() + position);
-		return card;
+		return cards[position];
 	}
 
-	int takeCard() {
-		int randomPos = rand() % cards.size();
-		return takeCard(randomPos);
-	}
+	int takeCard() {return takeCard((int) (rand() % cards.size()));}
 
 private:
 	void printVector(vector<int> v) {
@@ -122,6 +85,7 @@ private:
 	}
 
 	void printField(vector<vector<int> > field) {
+		cout << "\n";
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++)
 				cout << field[i][j] << "  ";
@@ -139,73 +103,123 @@ public:
 
 	void printActualGameField() {printField(actualGameField);}
 
-	bool getAllCardsFound() {return allCardsFound;}
-
-	bool pickTwoCards(int x1, int y1, int x2, int y2) { 
+	void readFirstCoords() {
 		try {
-			int card1 = pickCard(x1, y1);
-			int card2 = pickCard(x2, y2); 
+			string x, y;
+			
+			cin >> x >> y;
 
-			if (((x1 == x2) && (y1 == y2))) {
-				cout << "Positions must differ" << "\n";
-				return false;
-			}
-			if ((actualGameField[y1][x1] != 0) || (actualGameField[y2][x2] != 0)) {
-				cout << "You already guessed this card! Please pick a card you didn't guess yet" << "\n";
-				return false;
-			}
-
-			actualGameField[y1][x1] = gameField[y1][x1];
-			actualGameField[y2][x2] = gameField[y2][x2];
-
-			if (card1 == card2) {
-				ClearScreen();
-				cout << "Great Job! The cards are equal" << "\n";
-				printActualGameField();
-			} else {
-				ClearScreen();
-				cout << "The cards are not equal" << "\n";
-				printActualGameField();
-				actualGameField[y1][x1] = 0;
-				actualGameField[y2][x2] = 0;
-			}
-			return true;
+			x1 = readIntFromString(x);
+			y1 = readIntFromString(y);
 		} catch (const runtime_error& e) {
-			cout << "Please use valid coordinates" << "\n";
-			return false;
+			cout << "Please enter numbers instead of text";
+			readFirstCoords();
 		}
 	}
 
+	void readSecondCoords() {
+		try {
+			string x, y;
+			cin >> x >> y;
 
-	int pickCard(int x, int y) {
+			x2 = readIntFromString(x);
+			y2 = readIntFromString(y);
+		} catch (const runtime_error& e) {
+			cout << "Please enter numbers instead of text";
+			readSecondCoords();
+		}
+	}
+
+	int pickSecondCard() {
+		readSecondCoords();
+
+		if (x2 == x1 && y1 == y2) {
+			cout << "The coordinates of the cards are equal, please enter different coordinates" << "\n";
+			return pickSecondCard();
+		}
+
+		try {
+			return turnOverCard(x2, y2);
+		} catch (const runtime_error& e) {
+			cout << "Please enter valid coordinates" << "\n";
+			return pickSecondCard();
+		}
+	}
+
+	bool pickCards() {
+		cout << "Enter coordinates of the first card: ";
+		readFirstCoords();
+		int card1;
+
+		try {
+			card1 = turnOverCard(x1, y1);
+		} catch (const runtime_error& e) {
+			cout << "Please enter valid coordinates" << "\n";
+			return pickCards();
+		}
+		
+		ClearScreen();
+
+		actualGameField[y1][x1] = gameField[y1][x1];
+		cout << "Coordinates of the card: " << x1 << " " << y1 << "\n";
+		printActualGameField();
+
+		cout << "Enter coordinates of the second card: ";
+		int card2 = pickSecondCard();
+		ClearScreen();
+		
+		actualGameField[y2][x2] = gameField[y2][x2];
+		cout << "Coordinates of the first card: " << x1 << " " << y1 << "\n";
+		cout << "Coordinates of the second card: " << x2 << " " << y2 << "\n";
+		printActualGameField();
+
+		if (card1 == card2) {
+			cout << "Congradulations! The cards are equal!" << "\n";
+			allCards.erase(allCards.begin() + card1 - 1);
+			if (allCards.size() == 0) {
+				return true;
+			}
+			return false;
+		} else {
+			actualGameField[y1][x1] = 0;
+			actualGameField[y2][x2] = 0;
+			cout << "The cards differ!" << "\n";
+			return false;
+		}
+
+	}
+
+
+	int turnOverCard(int x, int y) {
 		if (x >= 0 and x < width and y >= 0 and y < height) {
-			return gameField[y][x];
-		} else throw runtime_error("Invalid coordinates (out of array length or less then zero)");
+			if (actualGameField[y][x] == 0)
+				return gameField[y][x];
+		}
+		throw runtime_error("Invalid coordinates");
 	}
 };
 
-
+// run the game
 bool execute(Field field, int limit) {
-	int x1, y1, x2, y2;
-	
-	bool validRound;
-	int attemp = 0;
-
-	while (true) {
-		cin >> x1 >> y1 >> x2 >> y2;
-
-		validRound = field.pickTwoCards(x1, y1, x2, y2);
-		
-		if (validRound) {
-			attemp++;
-			if (attemp == limit)
-				break;
-		}
-
-		if (field.getAllCardsFound())
+	for (int attemp = 0; attemp < limit; attemp++) {
+		if (field.pickCards())
 			return true;
 	}
+	cout << "\n" <<"!!!You have ran out of attempts!!!" << "\n";
 	return false;
+}
+
+// play the game once more/no
+bool playAgain() {
+	string answer;
+	cout << "\n" << "Play again? (y/n)" << "\n";
+	cin >> answer;
+
+	if (answer == "n")
+		return false;
+	else if (answer == "y") 
+		return true;
+	else return playAgain();
 }
 
 
@@ -214,44 +228,33 @@ int main()
 	// random seed init
 	srand(static_cast<unsigned int>(time(0)));
 
+	// sizes of the field, they are not customizable due to conditions of the task
 	int height = 4;
 	int width = 6;
-
 	cout << "The size of the field is " << height << " x " << width << "\n";
 
-	// limit of attempts
 	string input;
-
 	cout << "Please enter the amount of attempts you want to have: ";
 	cin >> input;
-
-	int limit = readIntFromString(input);
-
-	if (limit == 0) {
-		main();
-		return 0;
-	}
-
-
 	cout << "\n";
 
-	string answer;
+	int limit = readIntFromString(input);
+	if (limit < 1) {
+		main(); 
+		return 0;
+	}
 
 	while (true) {
 		Field field(height, width);
 		bool win = execute(field, limit);
-
-		if (win) {
-			cout << "Congradulations! You have a great memory!\nWant to play again? (y/n)\n";
-			cin >> answer;
-		} else {
-			cout << "This time you got unlucky :c\nBut you still have a chance to beat this game!\nWant to play again? (y/n)\n";
-			cin >> answer;
-		}
-		cout << "\n";
+		cout << "\n\n";
+		if (win)
+			cout << "Congradulations! You have a great memory!\nWant to play again?\n";
+		else 
+			cout << "This time you got unlucky :c\nBut you still have a chance to beat this game!\nWant to play again?\n";
 		
-		if (answer == "n") {
-			cout << "Hope you have spent your time nicely, see you soon c:" << "\n";
+		if (!playAgain()) {
+			cout << "\n" <<"Hope you have spent your time nicely, see you soon c:" << "\n";
 			break;
 		}
 	}
